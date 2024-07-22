b70c63625b717dd495de72bafbebb7e8d54a0f05
ES Oh <lspk.lpg@gmail.com>
2024-07-23 02:17:56 +0900

    implement ncp-dns-webhook

---
+++ main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/cert-manager/cert-manager/pkg/issuer/acme/dns/util"
+	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	"github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
+	"github.com/cert-manager/cert-manager/pkg/acme/webhook/cmd"
+	cmmetav1 "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
+	"github.com/es5h/ncp-dns-webhook/ncpdns"
+	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+)
+
+var GroupName = os.Getenv("GROUP_NAME")
+
+func main() {
+	if GroupName == "" {
+		panic("GROUP_NAME must be specified")
+	}
+
+	cmd.RunWebhookServer(GroupName,
+		&ncpDNSProviderSolver{},
+	)
+}
+
+type ncpDNSProviderSolver struct {
+	client       *kubernetes.Clientset
+	ncpDNSClient *ncpdns.NcpDnsClient
+}
+
+type ncpDNSProviderConfig struct {
+	AccessToken cmmetav1.SecretKeySelector `json:"accessTokenSecretRef"`
+	SecretToken cmmetav1.SecretKeySelector `json:"secretKeySecretRef"`
+	BaseURL     string                     `json:"baseUrl"`
+}
+
+func (c *ncpDNSProviderSolver) Name() string {
+	return "ncp-dns-solver"
+}
+
+func (c *ncpDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
+	cfg, err := loadConfig(ch.Config)
+	if err != nil {
+		return err
+	}
+
+	accessToken, err := c.loadSecretData(cfg.AccessToken, ch.ResourceNamespace)
+	if err != nil {
+		return err
+	}
+	secretKey, err := c.loadSecretData(cfg.SecretToken, ch.ResourceNamespace)
+	if err != nil {
+		return err
+	}
+
+	client := ncpdns.NewNcpDnsClient(ncpdns.NcpDnsOptions{
+		BaseUrl:   cfg.BaseURL,
+		AccessKey: string(accessToken),
+		SecretKey: string(secretKey),
+	})
+	c.ncpDNSClient = client
+
+	domainID, err := c.ncpDNSClient.GetDomainId(ch.ResolvedZone)
+	if err != nil {
+		return fmt.Errorf("ncpdns: error getting domain ID: %v", err)
+	}
+
+	err = c.ncpDNSClient.CreateTxtRecord(domainID, c.extractRecordName(ch.ResolvedFQDN, ch.ResolvedZone), ch.Key)
+	if err != nil {
+		return fmt.Errorf("ncpdns: error creating TXT record: %v", err)
+	}
+	return nil
+}
+
+func (c *ncpDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
+	domainID, err := c.ncpDNSClient.GetDomainId(ch.ResolvedZone)
+	if err != nil {
+		return fmt.Errorf("ncpdns: error getting domain ID: %v", err)
+	}
+
+	recordID, err := c.ncpDNSClient.GetTxtRecordId(domainID, c.extractRecordName(ch.ResolvedFQDN, ch.ResolvedZone))
+	if err != nil {
+		return fmt.Errorf("ncpdns: error getting TXT record ID: %v", err)
+	}
+
+	err = c.ncpDNSClient.DeleteTxtRecord(domainID, []int{recordID})
+	if err != nil {
+		return fmt.Errorf("ncpdns: error deleting TXT record: %v", err)
+	}
+	return nil
+}
+
+func (c *ncpDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
+	cl, err := kubernetes.NewForConfig(kubeClientConfig)
+	if err != nil {
+		return err
+	}
+
+	c.client = cl
+
+	return nil
+}
+
+func loadConfig(cfgJSON *extapi.JSON) (ncpDNSProviderConfig, error) {
+	cfg := ncpDNSProviderConfig{}
+	if cfgJSON == nil {
+		return cfg, nil
+	}
+	if err := json.Unmarshal(cfgJSON.Raw, &cfg); err != nil {
+		return cfg, fmt.Errorf("error decoding solver config: %v", err)
+	}
+
+	return cfg, nil
+}
+
+func (c *ncpDNSProviderSolver) extractRecordName(fqdn, domain string) string {
+	name := util.UnFqdn(fqdn)
+	if idx := strings.Index(name, "."+domain); idx != -1 {
+		return name[:idx]
+	}
+	return name
+}
+
+func (c *ncpDNSProviderSolver) loadSecretData(selector cmmetav1.SecretKeySelector, ns string) ([]byte, error) {
+	secret, err := c.client.CoreV1().Secrets(ns).Get(context.TODO(), selector.Name, v1.GetOptions{})
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to load secret %q", ns+"/"+selector.Name)
+	}
+
+	if data, ok := secret.Data[selector.Key]; ok {
+		return data, nil
+	}
+
+	return nil, errors.Errorf("no key %q in secret %q", selector.Key, ns+"/"+selector.Name)
+}
---
+++ ncpdns/client.go
@@ -0,0 +1,204 @@
+package ncpdns
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type NcpDnsOptions struct {
+	BaseUrl   string
+	AccessKey string
+	SecretKey string
+}
+
+type NcpDnsClient struct {
+	client    *http.Client
+	baseUrl   string
+	accessKey string
+	secretKey string
+}
+
+func NewNcpDnsClient(options NcpDnsOptions) *NcpDnsClient {
+	return &NcpDnsClient{
+		client:    &http.Client{},
+		baseUrl:   options.BaseUrl,
+		accessKey: options.AccessKey,
+		secretKey: options.SecretKey,
+	}
+}
+
+func (c *NcpDnsClient) GetDomainId(domainName string) (int, error) {
+	url := fmt.Sprintf("%s/dns/v1/ncpdns/domain?page=0&size=20&domainName=%s", c.baseUrl, domainName)
+	req, err := c.createRequest("GET", url, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("failed to get domain id: %s", resp.Status)
+	}
+
+	var result struct {
+		Content []struct {
+			Id int `json:"id"`
+		} `json:"content"`
+	}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return 0, err
+	}
+
+	if len(result.Content) == 0 {
+		return 0, fmt.Errorf("no domain found")
+	}
+
+	return result.Content[0].Id, nil
+}
+
+func (c *NcpDnsClient) CreateTxtRecord(domainId int, name, value string) error {
+	url := fmt.Sprintf("%s/dns/v1/ncpdns/record/%d", c.baseUrl, domainId)
+	record := []map[string]interface{}{
+		{
+			"host":    name,
+			"type":    "TXT",
+			"content": value,
+			"ttl":     300,
+		},
+	}
+	req, err := c.createRequest("POST", url, record)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to create TXT record: %s", resp.Status)
+	}
+
+	return c.ApplyChallenge(domainId)
+}
+
+func (c *NcpDnsClient) DeleteTxtRecord(domainId int, recordIds []int) error {
+	url := fmt.Sprintf("%s/dns/v1/ncpdns/record/%d", c.baseUrl, domainId)
+	req, err := c.createRequest("DELETE", url, recordIds)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to delete TXT record: %s", resp.Status)
+	}
+
+	return c.ApplyChallenge(domainId)
+}
+
+func (c *NcpDnsClient) GetTxtRecordId(domainId int, name string) (int, error) {
+	url := fmt.Sprintf("%s/dns/v1/ncpdns/record/%d?page=0&size=1&recordType=TXT&searchContent=%s", c.baseUrl, domainId, name)
+	req, err := c.createRequest("GET", url, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("failed to get TXT record id: %s", resp.Status)
+	}
+
+	var result struct {
+		Content []struct {
+			Id int `json:"id"`
+		} `json:"content"`
+	}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return 0, err
+	}
+
+	if len(result.Content) == 0 {
+		return 0, fmt.Errorf("no TXT record found")
+	}
+
+	return result.Content[0].Id, nil
+}
+
+func (c *NcpDnsClient) ApplyChallenge(domainId int) error {
+	url := fmt.Sprintf("%s/dns/v1/ncpdns/record/apply/%d", c.baseUrl, domainId)
+	req, err := c.createRequest("PUT", url, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to apply challenge: %s", resp.Status)
+	}
+
+	return nil
+}
+
+func (c *NcpDnsClient) createRequest(method, url string, body interface{}) (*http.Request, error) {
+	var bodyBytes []byte
+	var err error
+
+	if body != nil {
+		bodyBytes, err = json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(bodyBytes))
+	if err != nil {
+		return nil, err
+	}
+
+	timestamp := time.Now().UnixMilli()
+	signature := c.makeSignature(method, req.URL.RequestURI(), timestamp)
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("x-ncp-apigw-timestamp", fmt.Sprintf("%d", timestamp))
+	req.Header.Add("x-ncp-iam-access-key", c.accessKey)
+	req.Header.Add("x-ncp-apigw-signature-v2", signature)
+
+	return req, nil
+}
+
+func (c *NcpDnsClient) makeSignature(httpMethod, uri string, timestamp int64) string {
+	message := fmt.Sprintf("%s %s\n%d\n%s", httpMethod, uri, timestamp, c.accessKey)
+	h := hmac.New(sha256.New, []byte(c.secretKey))
+	h.Write([]byte(message))
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
+}
